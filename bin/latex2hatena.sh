25d302a4e1d20342ec965d731a68cbbe5822dc57
NotLeonian <75620009+NotLeonian@users.noreply.github.com>
2025-12-02 01:30:26 +0900

    latex2hatena.sh内のコマンドを変更

--- bin/latex2hatena.sh
+++ bin/latex2hatena.sh
@@ -1,24 +1,40 @@
 #! /bin/bash
 # -o, --bibliography=, --csl=, -M enable-upload=true/false
 execdir="`dirname $0`"
-settingsdir="${execdir}/../settings"
 filterdir="${execdir}/../filter"
 
 function usage {
 cat <<EOF
-  convert LaTeX source to HTML with syntax provided by hatenablog.com 
+  convert LaTeX source to HTML with syntax provided by hatenablog.com
 
   Usage:
     $(basename ${0}) [options] INPUT.tex
-  
+
   Options:
     -o FILE                   output file path. if not specified, to Stdout
     --bibliography=FILE PATH  bib.file path
     -M ...                    pandoc meta file arguments
 EOF
 }
+
 if [ $# -eq 0 ]; then
   usage
   exit 1
 fi
-pandoc --mathjax --wrap=auto -F pandoc-crossref -F ${filterdir}/hateblo-filter.py -f latex -M reference-section-title='参考文献' -M crossrefYaml=${settingsdir}/pandoc-crossref-settings.yaml $@
+
+pandoc \
+  -f latex \
+  -t html5 \
+  --mathjax \
+  --wrap=auto \
+  -F pandoc-crossref \
+  --citeproc \
+  -F ${filterdir}/hateblo-filter.py \
+  -M reference-section-title='参考文献' \
+  -M figureTitle='図' \
+  -M figPrefix='図' \
+  -M tableTitle='表' \
+  -M tblPrefix='表' \
+  -M link-citations \
+  -M linkReferences \
+  $@
