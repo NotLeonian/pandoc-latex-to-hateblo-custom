4936bf272a5e79bf78826ae226ce0edd9caea878
NotLeonian <75620009+NotLeonian@users.noreply.github.com>
2025-12-02 01:17:38 +0900

    uvを導入し、普通のpandocとpandoc-crossrefで動作するように変更

--- filter/hateblo-filter.py
+++ filter/hateblo-filter.py
@@ -1,14 +1,11 @@
-#! /usr/bin/env python3
+#! /usr/bin/env -S uv run
 
 # from pandocfilters import toJSONFilter, Emph, Para
-import argparse
 import os
-import sys
 import warnings
 from pathlib import Path
 import json
 import panflute as pf
-from collections import OrderedDict
 import re
 
 import requests
@@ -26,14 +23,18 @@ from pangu import spacing
 # https://www.lisz-works.com/entry/python3-fotolife-upload
 
 
-class hatena_token():
-    def __init__(self, HATENA_USER, HATENA_BLOG, FOTO_API_KEY, FOTO_FOLDER='Hatena Blog'):
+class hatena_token:
+    def __init__(
+        self, HATENA_USER, HATENA_BLOG, FOTO_API_KEY, FOTO_FOLDER="Hatena Blog"
+    ):
         self.hatena_user = HATENA_USER
         self.hatena_blog = HATENA_BLOG
         self.foto_api_key = FOTO_API_KEY
         self.foto_folder = FOTO_FOLDER
-        self.endpoint_photo = 'https://f.hatena.ne.jp/atom/post/'
-        self.endpoint_blog = f'https://blog.hatena.ne.jp/{self.hatena_user}/{self.hatena_blog}/atom'
+        self.endpoint_photo = "https://f.hatena.ne.jp/atom/post/"
+        self.endpoint_blog = (
+            f"https://blog.hatena.ne.jp/{self.hatena_user}/{self.hatena_blog}/atom"
+        )
 
     def _wsse(self, hatena_user=None, foto_api_key=None):
         if not hatena_user:
@@ -43,35 +44,45 @@ class hatena_token():
         created_at = datetime.now().isoformat() + "Z"
         b_nonce = hashlib.sha1(str(random.random()).encode()).digest()
         b_digest = hashlib.sha1(
-            b_nonce + created_at.encode() + foto_api_key.encode()).digest()
+            b_nonce + created_at.encode() + foto_api_key.encode()
+        ).digest()
         return 'UsernameToken Username="{}", PasswordDigest="{}", Nonce="{}", Created="{}"'.format(
             hatena_user,
             base64.b64encode(b_digest).decode(),
             base64.b64encode(b_nonce).decode(),
-            created_at)
+            created_at,
+        )
 
-    def _create_image_xml(self, file_name_path, title='', to_folder=None):
+    def _create_image_xml(self, file_name_path, title="", to_folder=None):
         if not to_folder:
-            to_folder = self.foto_folder if self.foto_folder else 'Hatena Blog'
+            to_folder = self.foto_folder if self.foto_folder else "Hatena Blog"
         content_type = mimetypes.guess_type(file_name_path)[0]
         uploadData = base64.b64encode(file_name_path.read_bytes())
-        return f"""
+        return (
+            f"""
     <entry xmlns="http://purl.org/atom/ns#">
     <title>{title}</title>
-    <content type="{content_type}" mode="base64">""" + uploadData.decode() + f"""</content>
+    <content type="{content_type}" mode="base64">"""
+            + uploadData.decode()
+            + f"""</content>
     <dc:subject>{to_folder}</dc:subject>
     </entry>
     """
+        )
 
-    def post_hatenaphoto(self, file_name, title='', to_folder=None, *args):
+    def post_hatenaphoto(self, file_name, title="", to_folder=None, *args):
         file_name_path = Path(file_name)
-        r = requests.post(self.endpoint_photo, data=self._create_image_xml(
-            file_name_path, title, to_folder), headers={'X-WSSE': self._wsse(*args)})
+        r = requests.post(
+            self.endpoint_photo,
+            data=self._create_image_xml(file_name_path, title, to_folder),
+            headers={"X-WSSE": self._wsse(*args)},
+        )
         try:
             r.raise_for_status()
-        except:
+        except requests.exceptions.HTTPError as exc:
             raise requests.exceptions.RequestException(
-                f'Error!\nstatus_code: {r.status_code}\nmessage: {r.text}')
+                f"Error!\nstatus_code: {r.status_code}\nmessage: {r.text}"
+            ) from exc
         self.last_result = r
         return self
 
@@ -88,7 +99,10 @@ def filter_hatena_space(elem, doc):
         # 数値を数式に変換
         # なお、このとき括弧や句読点などが前後にあってスペースが入っていない場合を考慮する
         text = re.sub(
-            r'(?:(?<=\s)|(?<=^)|(?<=[^\x00-\x7F]))([+-]?\d+(\.\d+)?)(?:(?=\s)|(?=$)|(?=[^\x00-\x7F]))', r'[tex: \1]', text)
+            r"(?:(?<=\s)|(?<=^)|(?<=[^\x00-\x7F]))([+-]?\d+(\.\d+)?)(?:(?=\s)|(?=$)|(?=[^\x00-\x7F]))",
+            r"[tex: \1]",
+            text,
+        )
         return pf.Str(text)
 
 
@@ -97,8 +111,8 @@ def filter_hatena_toc(elem, doc):
     目次を挿入する場合ははてな記法で自動生成するように置き換え
     """
     if isinstance(elem, pf.RawBlock):
-        if elem.format == 'latex' and elem.text == r'\tableofcontents{}':
-            return pf.Plain(pf.RawInline('[:contents]'))
+        if elem.format == "latex" and elem.text == r"\tableofcontents{}":
+            return pf.Plain(pf.RawInline("[:contents]"))
 
 
 def filter_hatena_header_level(elem, doc):
@@ -114,16 +128,16 @@ def filter_hatena_link(elem, doc):
     ハイパーリンクをはてな記法に置き換え
     """
     if isinstance(elem, pf.Link):
-        if elem.url[0] != '#':
+        if elem.url[0] != "#":
             url_title = pf.stringify(elem).strip()
-            if url_title == ':title:':
-                return pf.RawInline('[{}:title]'.format(elem.url))
-            if url_title == ':embed:':
-                return pf.RawInline('[{}:embed:title]'.format(elem.url))
-            elif url_title == '':
-                return pf.RawInline('[{}]'.format(elem.url))
+            if url_title == ":title:":
+                return pf.RawInline("[{}:title]".format(elem.url))
+            if url_title == ":embed:":
+                return pf.RawInline("[{}:embed:title]".format(elem.url))
+            elif url_title == "":
+                return pf.RawInline("[{}]".format(elem.url))
             else:
-                return pf.RawInline('[{0}:title={1}]'.format(elem.url, url_title))
+                return pf.RawInline("[{0}:title={1}]".format(elem.url, url_title))
 
 
 def filter_hatena_footnote(elem, doc):
@@ -131,33 +145,45 @@ def filter_hatena_footnote(elem, doc):
     脚注をはてな記法に置き換え. <code> が含まれていると機能しないので平文に変換する.
     """
     if isinstance(elem, pf.Note):
-        content_without_code = [pf.Str(f'`{pf.stringify(x)}`') if isinstance(
-            x, pf.Code) else x for x in elem.content[0].content]
-        return [pf.Str('((')] + content_without_code + [pf.Str('))')]
+        content_without_code = [
+            pf.Str(f"`{pf.stringify(x)}`") if isinstance(x, pf.Code) else x
+            for x in elem.content[0].content
+        ]
+        return [pf.Str("((")] + content_without_code + [pf.Str("))")]
 
 
 def filter_hatena_mathjax(elem, doc):
     def convert_math_symbols(text, is_displaymath=False):
         math_expr = text
-        math_expr = re.sub('^\\\\begin{aligned}', r'\\begin{align}', math_expr)
-        math_expr = re.sub('\\\\end{aligned}', r'\\end{align}', math_expr)
-        math_expr = re.sub(r'\[', r'\\[', math_expr)
-        math_expr = re.sub(r'\]', r'\\]', math_expr)
-        math_expr = re.sub(r'(?<!\\)_', ' _ ', math_expr)
-        math_expr = re.sub(r'(?<!\\)\^', ' ^ ', math_expr)
-        math_expr = re.sub('<', r'\\lt ', math_expr)
-        math_expr = re.sub('>', r'\\gt ', math_expr)
-        return ' [tex: {}{}] '.format('\\displaystyle ' if is_displaymath else "", math_expr)
+        math_expr = re.sub("^\\\\begin{aligned}", r"\\begin{align}", math_expr)
+        math_expr = re.sub("\\\\end{aligned}", r"\\end{align}", math_expr)
+        math_expr = re.sub(r"\[", r"\\[", math_expr)
+        math_expr = re.sub(r"\]", r"\\]", math_expr)
+        math_expr = re.sub(r"(?<!\\)_", " _ ", math_expr)
+        math_expr = re.sub(r"(?<!\\)\^", " ^ ", math_expr)
+        math_expr = re.sub("<", r"\\lt ", math_expr)
+        math_expr = re.sub(">", r"\\gt ", math_expr)
+        return " [tex: {}{}] ".format(
+            "\\displaystyle " if is_displaymath else "", math_expr
+        )
 
-    if isinstance(elem, pf.Math) and elem.format == 'InlineMath':
+    if isinstance(elem, pf.Math) and elem.format == "InlineMath":
         return pf.RawInline(convert_math_symbols(elem.text))
 
-    if isinstance(elem, pf.Para) and any(isinstance(content, pf.Math) and content.format == 'DisplayMath' for content in elem.content):
+    if isinstance(elem, pf.Para) and any(
+        isinstance(content, pf.Math) and content.format == "DisplayMath"
+        for content in elem.content
+    ):
         result_elem = pf.Para()
         for content in elem.content:
-            if isinstance(content, pf.Math) and content.format == 'DisplayMath':
-                result_elem.content.append(pf.RawInline(
-                    '<div class="Math DisplayMath" style="text-align: center;"> ' + convert_math_symbols(content.text, True) + ' </div>'))
+            if isinstance(content, pf.Math) and content.format == "DisplayMath":
+                result_elem.content.append(
+                    pf.RawInline(
+                        '<div class="Math DisplayMath" style="text-align: center;"> '
+                        + convert_math_symbols(content.text, True)
+                        + " </div>"
+                    )
+                )
             else:
                 result_elem.content.append(content)
         return result_elem
@@ -165,35 +191,49 @@ def filter_hatena_mathjax(elem, doc):
 
 def filter_hatena_blockquote(elem, doc):
     if isinstance(elem, pf.BlockQuote):
-        quotecomponents = [pf.RawInline('>>'), pf.RawInline(
-            '\n')] + list(elem.content[0].content) + [pf.RawInline('\n'), pf.RawInline('<<')]
+        quotecomponents = (
+            [pf.RawInline(">>"), pf.RawInline("\n")]
+            + list(elem.content[0].content)
+            + [pf.RawInline("\n"), pf.RawInline("<<")]
+        )
         return pf.Plain(*quotecomponents)
-    elif isinstance(elem, pf.Div) and 'epigraph' in elem.classes:
+    elif isinstance(elem, pf.Div) and "epigraph" in elem.classes:
         epigraph_phrase = elem.content[0]
         epigraph_source = elem.content[1]
-        return pf.Plain(*[pf.RawInline(f'>')] + list(epigraph_source.content) + [pf.RawInline('>'), pf.RawInline('\n')] + list(epigraph_phrase.content) + [pf.RawInline('\n'), pf.RawInline('<<')])
+        return pf.Plain(
+            *[pf.RawInline(">")]
+            + list(epigraph_source.content)
+            + [pf.RawInline(">"), pf.RawInline("\n")]
+            + list(epigraph_phrase.content)
+            + [pf.RawInline("\n"), pf.RawInline("<<")]
+        )
 
 
 def filter_hatena_codeblock(elem, doc):
     if isinstance(elem, pf.CodeBlock):
         # TODO: デフォルト言語をどこで読み取るか
-        lang = elem.attributes.get('language')
-        return pf.RawBlock('>|{lang}|\n{code}\n||<'.format(code=elem.text, lang=lang if lang else ''))
+        lang = elem.attributes.get("language")
+        return pf.RawBlock(
+            ">|{lang}|\n{code}\n||<".format(code=elem.text, lang=lang if lang else "")
+        )
 
 
 def filter_hatena_image(elem, doc):
     """
     pandoc-crossref のタグを消したりはてなフォトライフにアップロードしたりはてな記法に置換したり
     """
-    enable_upload = doc.get_metadata('enable-upload')
+    enable_upload = doc.get_metadata("enable-upload")
     if not enable_upload:
         enable_upload = False
 
     if isinstance(elem, pf.Image):
         if enable_upload:
-            settings_local = Path().cwd().joinpath('settings.json')
-            settings_root = Path(__file__).resolve(
-            ).parent.parent.joinpath('settings/settings.json')
+            settings_local = Path().cwd().joinpath("settings.json")
+            settings_root = (
+                Path(__file__)
+                .resolve()
+                .parent.parent.joinpath("settings/settings.json")
+            )
             warnings.warn(settings_local)
             warnings.warn(settings_root)
             if settings_local.exists():
@@ -203,35 +243,53 @@ def filter_hatena_image(elem, doc):
             else:
                 path_settings = None
             if path_settings is not None:
-                with settings_root.open('r') as f:
+                with settings_root.open("r") as f:
                     params_hatenaapi = json.load(f)
             else:
-                params_hatenaapi = {k: os.environ.get(k) for k in (
-                    'FOTO_API_KEY', 'HATENA_USER', 'HATENA_BLOG')}
-            for k in ('FOTO_API_KEY', 'HATENA_USER', 'HATENA_BLOG'):
+                params_hatenaapi = {
+                    k: os.environ.get(k)
+                    for k in ("FOTO_API_KEY", "HATENA_USER", "HATENA_BLOG")
+                }
+            for k in ("FOTO_API_KEY", "HATENA_USER", "HATENA_BLOG"):
                 if params_hatenaapi.get(k) is None:
-                    raise KeyError(
-                        f'API Parameter `{k}` not found in the settings.')
+                    raise KeyError(f"API Parameter `{k}` not found in the settings.")
             uploader = hatena_token(**params_hatenaapi)
             uploader.post_hatenaphoto(elem.url)
             res = ElementTree.fromstring(uploader.last_result.text)
-            image_file_id = ':'.join(res.find('hatena:syntax', {
-                                     'hatena': 'http://www.hatena.ne.jp/info/xmlns#'}).text.split(':')[2:-1])
-            image_file_id = f'[f:id:{image_file_id}:plain]'  # TODO: 画像サイズ調整
+            image_file_id = ":".join(
+                res.find(
+                    "hatena:syntax", {"hatena": "http://www.hatena.ne.jp/info/xmlns#"}
+                ).text.split(":")[2:-1]
+            )
+            image_file_id = f"[f:id:{image_file_id}:plain]"  # TODO: 画像サイズ調整
         else:
-            image_file_id = 'INSERT_FILE_ID_HERE'
-        ref_ids_img = [x.identifier for x in elem.content if hasattr(
-            x, 'identifier') and x.identifier[:4] == 'fig:']
+            image_file_id = "INSERT_FILE_ID_HERE"
+        ref_ids_img = [
+            x.identifier
+            for x in elem.content
+            if hasattr(x, "identifier") and x.identifier[:4] == "fig:"
+        ]
         if len(ref_ids_img) > 0:  # TODO: 複数ID存在するケースってあるの
+            caption_inlines = [x for x in elem.content if isinstance(x, pf.Inline)]
             caption_inlines = [
-                x for x in elem.content if isinstance(x, pf.Inline)]
-            caption_inlines = [x for x in caption_inlines if not (
-                hasattr(x, 'attributes') and x.attributes.get('label'))]
+                x
+                for x in caption_inlines
+                if not (hasattr(x, "attributes") and x.attributes.get("label"))
+            ]
             caption_plain = str(pf.stringify(pf.Span(*caption_inlines)))
-            return [
-                pf.RawInline('\n'), pf.RawInline(
-                    f'><figure class="figure-image figure-image-fotolife" title="{caption_plain}"><figcaption id="{ref_ids_img[0]}">')
-            ] + caption_inlines + [pf.RawInline(f'</figcaption>{image_file_id}</figure><'), pf.RawInline('\n')]
+            return (
+                [
+                    pf.RawInline("\n"),
+                    pf.RawInline(
+                        f'><figure class="figure-image figure-image-fotolife" title="{caption_plain}"><figcaption id="{ref_ids_img[0]}">'
+                    ),
+                ]
+                + caption_inlines
+                + [
+                    pf.RawInline(f"</figcaption>{image_file_id}</figure><"),
+                    pf.RawInline("\n"),
+                ]
+            )
         else:
             return pf.RawInline(image_file_id)
 
@@ -241,13 +299,18 @@ def filter_table_remove_tag(elem, doc):
     pandoc-crossref が残すタグを消す
     """
     if isinstance(elem, pf.Table):
-        ref_ids_tab = [x.identifier for x in elem.caption if hasattr(
-            x, 'identifier') and x.identifier[:4] == 'tab:']
+        ref_ids_tab = [
+            x.identifier
+            for x in elem.caption.content
+            if hasattr(x, "identifier") and x.identifier[:4] == "tab:"
+        ]
         if len(ref_ids_tab) > 0:
+            tab_caption_inlines = [x for x in elem.caption.content if isinstance(x, pf.Inline)]
             tab_caption_inlines = [
-                x for x in elem.caption if isinstance(x, pf.Inline)]
-            tab_caption_inlines = [x for x in tab_caption_inlines if not (
-                hasattr(x, 'attributes') and x.attributes.get('label'))]
+                x
+                for x in tab_caption_inlines
+                if not (hasattr(x, "attributes") and x.attributes.get("label"))
+            ]
             elem.caption = tab_caption_inlines
             return elem
 
@@ -256,22 +319,28 @@ def filter_eqref(elem, doc):
     """
     [eq:...] の参照タグを MathJax 参照に置き換える
     """
-    if isinstance(elem, pf.Link) and elem.url[:4] == '#eq:':
+    if isinstance(elem, pf.Link) and elem.url[:4] == "#eq:":
         ref_id_eq = elem.url[1:]
-        return pf.Span(pf.RawInline('(\\ref{' + ref_id_eq + '})'), attributes={'data-reference-type': 'ref', 'data-reference': f'{ref_id_eq}'})
+        return pf.Span(
+            pf.RawInline("(\\ref{" + ref_id_eq + "})"),
+            attributes={"data-reference-type": "ref", "data-reference": f"{ref_id_eq}"},
+        )
 
 
-if __name__ == '__main__':
-    pf.run_filters(actions=[
-        filter_hatena_space,
-        filter_hatena_toc,
-        filter_hatena_header_level,
-        filter_hatena_link,
-        filter_hatena_footnote,
-        filter_hatena_mathjax,
-        filter_hatena_blockquote,
-        filter_hatena_codeblock,
-        filter_hatena_image,
-        filter_table_remove_tag,
-        filter_eqref
-    ], doc=None)
+if __name__ == "__main__":
+    pf.run_filters(
+        actions=[
+            filter_hatena_space,
+            filter_hatena_toc,
+            filter_hatena_header_level,
+            filter_hatena_link,
+            filter_hatena_footnote,
+            filter_hatena_mathjax,
+            filter_hatena_blockquote,
+            filter_hatena_codeblock,
+            filter_hatena_image,
+            filter_table_remove_tag,
+            filter_eqref,
+        ],
+        doc=None,
+    )
